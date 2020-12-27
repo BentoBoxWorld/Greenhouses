7fb5313537cd982e43afe596146ff5a87425734d
tastybento <tastybento@wasteofplastic.com>
2020-12-27 11:17:56 -0800

    Version 1.4.2 Update to BentoBox API 1.15.4

--- src/main/java/world/bentobox/greenhouses/listeners/IslandChangeEvents.java
+++ src/main/java/world/bentobox/greenhouses/listeners/IslandChangeEvents.java
@@ -4,7 +4,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
-import world.bentobox.bentobox.api.events.island.IslandEvent.IslandDeleteEvent;
+import world.bentobox.bentobox.api.events.island.IslandDeleteEvent;
 import world.bentobox.greenhouses.Greenhouses;
 
 /**
