db5ef4d5dadfd7753b3d2b46d41c9f204a112b43
BONNe <bonne@bonne.id.lv>
2023-04-08 18:39:46 +0300

    Create plugin.yml (#106)
    * Create plugin.yml
    
    * Update pom.xml
    
    * Update GreenhousesPladdon.java

--- src/main/java/world/bentobox/greenhouses/GreenhousesPladdon.java
+++ src/main/java/world/bentobox/greenhouses/GreenhousesPladdon.java
@@ -1,9 +1,6 @@
 package world.bentobox.greenhouses;
 
 
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
-
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
@@ -11,8 +8,6 @@ import world.bentobox.bentobox.api.addons.Pladdon;
 /**
  * @author tastybento
  */
-@Plugin(name = "Greenhouses", version = "1.0")
-@ApiVersion(ApiVersion.Target.v1_18)
 public class GreenhousesPladdon extends Pladdon
 {
     @Override
